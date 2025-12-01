cd1a5c78f9bf42434a27f03cea6b9f32dcd058c0
manuelnaldi <mnaldi.business@gmail.com>
2025-12-01 16:32:55 +0100

    feat: add solution for day1 - part2

--- src/Days/Day01/day01.ts
+++ src/Days/Day01/day01.ts
@@ -4,24 +4,33 @@ type Direction = `${'L' | 'R'}${number}`;
 
 const useDial = () => {
     var arrowPointer = 50;
-    var password = 0;
+    var passwordPartOne = 0;
+    var passwordPartTwo = 0;
 
     function movePointer(d: Direction){
         const rotation = d.charAt(0) as "L" | "R";
         const distance = Number(d.substring(1));
-        const delta = distance % 100
+        const fullCycles = Math.floor(distance / 100);
+        const delta = distance % 100;
 
-        rotation === "L" 
-            ? arrowPointer = (arrowPointer - delta % 100 + 100) % 100
-            : arrowPointer = (arrowPointer + delta) % 100
+        if (rotation === "L") {
+            arrowPointer === 0 
+                ? passwordPartTwo += fullCycles
+                : passwordPartTwo += fullCycles + (delta >= arrowPointer ? 1 : 0);
+            
+            arrowPointer = (arrowPointer - delta + 100) % 100;
+        } else {
+            passwordPartTwo += Math.floor((arrowPointer + distance) / 100);
+            arrowPointer = (arrowPointer + delta) % 100;
+        }
 
-        if(arrowPointer === 0) password++;
+        if (arrowPointer === 0) passwordPartOne++;
     }
 
     return {
         movePointer,
-        getPassword: () => password,
-        getArrowPointer: () => arrowPointer
+        getPasswordPartOne: () => passwordPartOne,
+        getPasswordPartTwo: () => passwordPartTwo,
     }
 }
 
@@ -30,8 +39,8 @@ const dial = useDial();
 const fileReader = useFileReader();
 const directions = fileReader.readFileLines("input.txt") as Direction[]
 
-// part one
 directions.forEach(d => {
     dial.movePointer(d);
 });
-console.log("Password: "+ dial.getPassword());
+console.log("Password (1): "+ dial.getPasswordPartOne());
+console.log("Password (2): "+ dial.getPasswordPartTwo());
