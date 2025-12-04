96311c6743fd6df2672d5b8a6728917d571b6630
manuelnaldi <mnaldi.business@gmail.com>
2025-12-04 16:10:07 +0100

    feat: add solution for day4 - part2

--- src/Days/Day04/day04.ts
+++ src/Days/Day04/day04.ts
@@ -1,14 +1,17 @@
 import useFileReader from "../../utils/fileReader";
 
 const useForkLifts = () => {
-    var accessibleRolls = 0;
+    var accessibleRollsPart2 = 0;
+    var internalGrid;
 
-    const checkAccessibleRolls = (grid) => {
+    const checkAccessibleRolls = (grid: string[][]) => {
+        internalGrid = structuredClone(grid);
         const directions = [
             [-1, -1], [-1, 0], [-1, 1],
             [0, -1],           [0, 1],
             [1, -1],  [1, 0],  [1, 1]
         ];
+        let removableRolls = 0;
 
         for (let x = 0; x < grid.length; x++) {
             for (let y = 0; y < grid[x].length; y++) {
@@ -18,14 +21,29 @@ const useForkLifts = () => {
                     return (grid[x + dx]?.[y + dy] === "@") ? count + 1 : count;
                 }, 0);
 
-                if (adjacentRolls < 4) accessibleRolls++;
+                if (adjacentRolls < 4) {
+                    removableRolls++;
+                    internalGrid[x][y] = "x"; 
+                };
             }
         }
+        return removableRolls;
     };
+
+    const removeAndCountAccessibleRolls = (grid: string[][]) => {
+        internalGrid = structuredClone(grid);
+        let removedRolls = -1;
+        while (removedRolls !== 0){
+            removedRolls = checkAccessibleRolls(internalGrid);
+            accessibleRollsPart2 += removedRolls;
+        }
+
+    }
 
     return {
         checkAccessibleRolls,
-        getAccessibleRolls: () => accessibleRolls
+        removeAndCountAccessibleRolls,
+        getAccessibleRollsPart2: () => accessibleRollsPart2,
     }
 }
 
@@ -33,11 +51,13 @@ const useForkLifts = () => {
 const forklifts = useForkLifts();
 const lines = useFileReader().readFile("input.txt").split("\n");
 const grid: string[][] = [];
-
 lines.forEach((line) => {
     const rolls = line.split("");
     grid.push(rolls);
 });
-forklifts.checkAccessibleRolls(grid);
 
-console.log("total accessible rolls (1): " + forklifts.getAccessibleRolls());
+const accessibleRolls = forklifts.checkAccessibleRolls(grid);
+forklifts.removeAndCountAccessibleRolls(grid);
+
+console.log("total accessible rolls (1): " + accessibleRolls);
+console.log("total removed rolls (2): " + forklifts.getAccessibleRollsPart2());
